7ae00f446d615aae1f8654e9be408904e178b152
DOGHOUSETV <arubigeorge0@gmail.com>
2021-10-10 00:13:36 -0400

    Creating navigation bar

---
+++ src/Navigation Bar/NavBar.js
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+function NavBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">l</div>
+            <div className="topCenter">c</div>
+            <div className="topRight">r</div>
+        </div>
+    )
+}
+
+export default NavBar
