691ee46a3b93ebde3097d7309480c94c1f8398e4
DOGHOUSETV <arubigeorge0@gmail.com>
2021-10-11 10:06:37 -0400

    Commit

---
+++ src/Navigation Bar/NavBar.js
@@ -0,0 +1,16 @@
+import './navbar.css'
+import InstagramIcon from '@mui/icons-material/Instagram'
+
+function NavBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <InstagramIcon />
+            </div>
+            <div className="topCenter">c</div>
+            <div className="topRight">r</div>
+        </div>
+    )
+}
+
+export default NavBar
