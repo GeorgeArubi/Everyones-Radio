343f24436f2b88a39c2a571f2f6aa77d49db18f5
DOGHOUSETV <arubigeorge0@gmail.com>
2021-10-11 10:04:41 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import NavBar from "./Navigation Bar/NavBar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-    <NavBar />
-    
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
