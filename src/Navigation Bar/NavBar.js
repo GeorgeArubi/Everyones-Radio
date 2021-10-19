7db70a9e1292e27e3842de26f807b66746b1228b
DOGHOUSETV <arubigeorge0@gmail.com>
2021-10-18 21:38:06 -0400

    navigation bar updates

--- src/Navigation Bar/NavBar.js
+++ src/Navigation Bar/NavBar.js
@@ -1,14 +1,29 @@
 import './navbar.css'
-//import InstagramIcon from '@mui/icons-material/Instagram'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 function NavBar() {
     return (
         <div className="top">
             <div className="topLeft">
-                l
+                <InstagramIcon />
+                <TwitterIcon />
             </div>
-            <div className="topCenter">c</div>
-            <div className="topRight">r</div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img
+                    src="https://github.com/GeorgeArubi/Everyones-Radio/blob/master/media_assets/Logo%202.PNG"
+                    alt="logo"
+                />
+            </div>
         </div>
     )
 }
