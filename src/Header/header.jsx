1ab092e5f435073be3a1afacf4ce2ae966704317
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 12:46:41 -0400

    Update header.jsx

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -8,7 +8,7 @@ function header() {
                 <span className="headerTitleLg">Blog</span>
             </div>
             <img 
-                className="headerLogo" 
+                className="headerImg" 
                 src="https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
                 alt="concert" 
             />
