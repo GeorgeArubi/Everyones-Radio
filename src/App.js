2a6a48470950a0a5981d4dca01ce8fbf8a40ca3f
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 03:42:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import NavBar from "./Navigation Bar/NavBar";
+import Home from './Pages/Home/Home'
+import NavBar from './Navigation Bar/NavBar'
 
 function App() {
   return (
     <>
     <NavBar />
+    <Home />
     </>
   );
 }
