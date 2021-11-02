cf52f55bc70be467c22ed809adcc15ba8f5805d1
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 03:43:03 -0400

    Update header.jsx

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,9 +1,17 @@
-import './header.css'
+import './Header.css'
 
 function header() {
     return (
         <div className="header">
-            
+            <div className="headerTitles">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+            </div>
+            <img 
+                className="headerLogo" 
+                src="https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                alt="concert" 
+            />
         </div>
     )
 }
