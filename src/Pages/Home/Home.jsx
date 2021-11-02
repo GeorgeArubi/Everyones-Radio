86e8f1319bc6d976d911a8d1042794ff421507b6
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 14:50:35 -0400

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
-import Header from '../../Header/Header';
-import './Home.css'
+import Header from '../../header/Header'
+import Posts from '../../posts/Posts'
+import Sidebar from '../../sidebar/Sidebar'
+import './home.css'
 
 function Home() {
     return (
-        <div className="home">
+        <>
             <Header />
-            homepage
-        </div>
+        
+            <div className="home">
+                <Posts />
+                <Sidebar />
+            </div>
+        </>
     )
 }
 
