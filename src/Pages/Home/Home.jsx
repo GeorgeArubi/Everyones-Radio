b632b83e0e2bbde8d5292d81be3478ae8ba6226f
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 03:43:11 -0400

    Create Home.jsx

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../../Header/Header';
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <Header />
+            homepage
+        </div>
+    )
+}
+
+export default Home
