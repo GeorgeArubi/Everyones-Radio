bc2569aa4ec5ac11fd9008e1ad53fd20e952926b
DOGHOUSETV <arubigeorge0@gmail.com>
2021-11-02 15:17:03 -0400

    Update SideBar.jsx

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -6,9 +6,14 @@ function Sidebar() {
             <div className="sidebarItem">
                 <span className="sidebarTitle">ABOUT ME</span>
                 <img 
+                    className="sidebarImage"
                     src="https://i.pinimg.com/564x/c7/60/74/c76074651fc8831810961cdda3896c5f.jpg" 
                     alt="about"
                 />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, expedita.
+                    Ut praesentium, aliquam exercitationem nam consectetur asperiores nisi. 
+                </p>
             </div> 
         </div>
     )
