abc450aca8364b1c1c7bff8b01e1df613179eebc
DOGHOUSETV <arubigeorge0@gmail.com>
2022-03-14 09:55:28 -0400

    Landing page after user registration

---
+++ components/Playlist.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Playlist = () => {
+  return (
+    <div class="flex items-center justify-center h-screen">
+      <div class="w-96 bg-white shadow rounded">
+        <div class="grid grid-col gap-2 place-content-center">
+          <div>Everyone's Radio (Beta)</div>
+          <div>Thank you message</div>
+          <div>Sounds like anxiety cover</div>
+          <div>Apple Music & Spotify Links (Buttons)</div>
+          <div>About this project link</div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Playlist
