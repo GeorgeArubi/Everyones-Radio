8c458be8c7136b509d8ef166a1fea04486e62f3b
DOGHOUSETV <arubigeorge0@gmail.com>
2022-03-19 01:30:14 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,32 +1,34 @@
 import React from 'react'
-import { getProviders, signIn} from 'next-auth/react'
+import { getProviders, signIn } from 'next-auth/react'
 import dynamic from 'next/dynamic'
 
 const WebGL = dynamic(() => import('../components/WebGL'), {
   ssr: false
 })
 
-const Login = ( { providers }) => {
+const Login = ({ providers }) => {  
   return (
     <>
       <WebGL />
       <div className="overlay">
-          <div className="overlay__inner">
-              <h1 className="overlay__title text-gradient">Everyone's Radio (Beta)</h1>
-              <p className="overlay__description">
-                Welcome to ...
-              </p>
-              <div className="overlay__btns">
-                <button className="overlay__btn overlay__btn--transparent">Apple Music</button>
-                <button className="overlay__btn overlay__btn--colors">Spotify</button>
+        <div className="overlay__inner">
+          <h1 className="overlay__title text-gradient">Everyone's Radio (Beta)</h1>
+          <p className="overlay__description">
+            Welcome to ...
+          </p>
+        <div className="overlay__btns">
+          <button className="overlay__btn overlay__btn--transparent">Apple Music</button>
+            {Object.values(providers).map((provider) => (
+              <div className="overlay__btn overlay__btn--colors" key={provider.name}>
+                <button className="spotify__login">{provider.name}</button>
               </div>
-            </div>
+            ))}
+          </div>
         </div>
+      </div>
     </>
   )
 }
-
-export default Login
 
 export async function getServerSideProps() {
   const providers = await getProviders()
@@ -37,3 +39,5 @@ export async function getServerSideProps() {
     }
   }
 }
+
+export default Login
