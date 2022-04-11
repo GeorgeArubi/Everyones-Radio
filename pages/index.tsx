45297a9ef1012c98c2e608fd80fa070dfb7a2403
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-11 12:08:03 -0400

    Restructure landing page components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import type { NextPage } from 'next'
-import Playlist from '../components/Playlist'
+import dynamic from 'next/dynamic'
+import Playlists from '../components/Playlists'
+import Sidebar from './../components/Sidebar'
+
+const WebGL = dynamic(() => import('./../components/WebGL'), {
+  ssr: false
+})
 
 const Home: NextPage = () => {
+  
   return (
-    <div className="h-screen overflow-hidden">
-        <Playlist />
-    </div>
+    <>
+      <WebGL />
+      <div className="h-screen overflow-hidden">
+        <div className="w-full h-5/6 pt-32 pb-32 pr-24 pl-24 
+                        flex items-center rounded-[2rem] 
+                        bg-white/[0.375] shadow-[0_0.75rem_2rem_0_rgba(0,0,0,0.1)]
+                        border border-solid border-[#ffffff20]">
+                          
+          <Sidebar />
+          <Playlists />
+        </div>
+      </div>
+    </>
+    
   )
 }
 
