9a3e2c39b967b03501753294c63e9f9891c7a24a
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-13 10:39:18 -0400

    Custom spotify api hook

---
+++ hooks/useSpotify.ts
@@ -0,0 +1,20 @@
+import { useSession, signIn } from 'next-auth/react'
+import { useEffect } from 'react'
+import spotifyAPI from '../lib/spotify'
+
+const useSpotify = () => {
+  const { data: session } = useSession()
+  useEffect(() => {
+    if (session) {
+      // If refresh access token attempts to fail, direct the user to the login page
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn()
+      }
+      spotifyAPI.setAccessToken(session.user?.accessToken)
+    }
+  }, [session])
+
+  return spotifyAPI
+}
+
+export default useSpotify
