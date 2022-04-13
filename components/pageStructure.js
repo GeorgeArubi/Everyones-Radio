e304b653bba86f4b6309e7ab5d34baeeee55184a
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-13 10:37:47 -0400

    Placeholder component

--- components/pageStructure.js
+++ components/pageStructure.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { shuffle } from 'lodash'
+
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-red-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+]
 
 const pageStructure = () => {
+  const { data: session } = useSession()
+  const [color, setColor] = useState(null)
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [])
+
   return (
-    <div>pageStructure</div>
+    <div className="flex-grow">
+      <header className="absolute top-5 right-8">
+        <div className="flex items-center bg-black space-x-3
+        opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+          <img 
+            className="rounded-full w-10 h-10" 
+            src={session?.user.image}  
+            alt=""
+          />
+          <h2>{session?.user.name}</h2>
+          <ChevronDownIcon className="h-5 w-5" />
+        </div>
+      </header>
+
+      <section className={`flex items-end space-x-7 
+      bg-gradient-to-b to-black ${color} h-80 text=white p-8`}>
+
+      </section>
+    </div>
   )
 }
 
