a259064b67d18d34dcbc741388ead2a7b7f68311
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-15 11:19:49 -0400

    Use recoil for state management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import './style.css'
 import type {AppProps} from 'next/app';
 import {SessionProvider} from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 
 
 function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
