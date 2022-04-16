2a1fb7cd400c817652b09f1e47255f814ea0f94f
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-16 10:43:44 -0400

    Test spotify playlist components

---
+++ components/Center.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import Songs from './Songs'
+import { useSession } from 'next-auth/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { shuffle } from 'lodash'
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { playlistIdState, playlistState } from '../atoms/playlistAtoms';
+import useSpotify from '../hooks/useSpotify';
+
+const colors = [
+  "from-indigo-500",
+  "from-blue-500",
+  "from-green-500",
+  "from-red-500",
+  "from-yellow-500",
+  "from-pink-500",
+  "from-purple-500",
+]
+
+const Center = () => {
+  const { data: session } = useSession()
+  const spotifyApi = useSpotify()
+  const [color, setColor] = useState(null)
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop())
+  }, [playlistId])
+
+  useEffect(() => {
+    spotifyApi.getPlaylist(playlistId).then(data => {
+      setPlaylist(data.body)
+    })
+    .catch((err) => console.log("Something went wrong", err))
+  }, [spotifyApi, playlistId]) // Use playlistId to refetch data
+
+  console.log(playlist)
+
+  return (
+    <div className="flex-grow p-5 
+    overflow-y-scroll h-full scrollbar-hide relative">
+      <div className="absolute py-3 top-5 right-8">
+        <div className="flex items-center bg-black space-x-3
+        opacity-90 hover:opacity-80 cursor-pointer rounded-full 
+        p-1 pr-2 text-white w-60">
+          <img 
+            className="rounded-full w-10 h-10" 
+            src={session?.user.image}  
+            alt=""
+          />
+          <h2>{session?.user.name}</h2>
+          <ChevronDownIcon className="h-5 w-5" />
+        </div>
+      </div>
+
+      <section className={`flex items-end space-x-7 
+      bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
+        <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="Playlist Cover"/>
+        <div>
+          <p>PLAYLIST</p>
+          <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>{playlist?.name}</h1>
+        </div>
+      </section>
+
+      <div>
+        <Songs />
+      </div>
+    </div>
+  )
+}
+
+export default Center
