b8b98f9801547fee183af28d7663ad700317071d
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-17 18:27:07 -0400

    Render track details

--- components/Song.js
+++ components/Song.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import useSpotify from '../hooks/useSpotify'
+import { millisecondToMinutesAndSeconds } from '../lib/time_duration'
 
 const Song = ({ order, track }) => {
+  const spotifyApi = useSpotify()
+
   return (
-    <div>Song</div>
+    <div className="grid grid-cols-2 pt-2 text-gray-500">
+      <div className='flex items-center space-x-4'>
+        <p className='text-right'>{order + 1}</p>
+        <img
+          className='h-10 w-10'
+          src={track.track.album.images[0].url}
+          alt='Song Covers'
+        />
+        <div>
+          <p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
+          <p className='w-40'>{track.track.artists[0].name}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between ml-auto md:ml-0">
+        <p className="w-40 hidden md:inline">{track.track.album.name}</p>
+        <p>{millisecondToMinutesAndSeconds(track.track.duration_ms)}</p>
+      </div>
+    </div>
   )
 }
 
