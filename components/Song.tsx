0513b36298b14971471769e7632edb489a0571b9
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-17 22:24:20 -0400

    Use typescript syntax

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 import useSpotify from '../hooks/useSpotify'
 import { millisecondToMinutesAndSeconds } from '../lib/time_duration'
 
-const Song = ({ order, track }) => {
+const Song = ({ order, track }: {order: number, track: any}) => {
   const spotifyApi = useSpotify()
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+
+  const playSong = () => {
+    setCurrentTrackId(track.track.id)
+    setIsPlaying(true)
+    spotifyApi.play({
+      uris: [track.track.uri]
+    })
+  }
 
   return (
-    <div className="grid grid-cols-2 pt-2 text-gray-500">
+    <div 
+      className="grid grid-cols-2 py-2 px-5 text-gray-500 
+    hover:bg-white hover:bg-opacity-50 hover:backdrop-filter hover:backdrop-blur-lg rounded-lg cursor-pointer"
+      onClick={playSong}
+    >
       <div className='flex items-center space-x-4'>
         <p className='text-right'>{order + 1}</p>
         <img
@@ -15,7 +31,7 @@ const Song = ({ order, track }) => {
           alt='Song Covers'
         />
         <div>
-          <p className="w-36 lg:w-64 text-white truncate">{track.track.name}</p>
+          <p className="w-36 lg:w-64 text-black truncate">{track.track.name}</p>
           <p className='w-40'>{track.track.artists[0].name}</p>
         </div>
       </div>
--- components/Songs.tsx
+++ components/Songs.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useRecoilValue } from 'recoil';
 import { playlistState } from '../atoms/playlistAtoms';
-import Song from './Song'
+import Song from './Song';
 
 const Songs = () => {
-  const playlist = useRecoilValue(playlistState);
+  const playlist: any = useRecoilValue(playlistState);
 
   return (
-    <div className="px-8 flex flex-col space-y-1 text-gray-500">
-      {playlist?.tracks.items.map((track, i) => (
+    <div className="flex flex-col space-y-1 text-gray-500">
+      {playlist?.tracks.items.map((track: any, i: number) => (
         <Song key={track.track.id} track={track} order={i} />
       ))}
     </div>
