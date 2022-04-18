3d2efae366498071f5719415fad93d3dd61c2043
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-18 10:13:47 -0400

    Clean up styling imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import './style.css'
 import type {AppProps} from 'next/app';
 import {SessionProvider} from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
