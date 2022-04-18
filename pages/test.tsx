9cf2960e84d29ea2f78b294fa2fd1bdfef42232c
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-18 10:14:35 -0400

    Render music player in test environment

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,6 +3,7 @@ import { getSession } from 'next-auth/react'
 import dynamic from 'next/dynamic'
 import Center from '../components/Center'
 import Sidebar from './../components/Sidebar'
+import Player from './../components/Player'
 
 const WebGL = dynamic(() => import('./../components/WebGL'), {
   ssr: false
@@ -15,14 +16,18 @@ const Test: NextPage = () => {
       <WebGL />
       <div className="h-screen overflow-hidden">
         <main className="
-          w-full h-5/6 px-6 py-8 
+          w-full h-4/5 px-6 py-8 
           flex items-center rounded-[2rem] 
-        bg-white/[0.375] shadow-[0_0.75rem_2rem_0_rgba(0,0,0,0.1)]
+        bg-white/[0.375] shadow-[0_12px_20px_-1px_rgba(0,0,0,0.1)]
           border border-solid border-[#ffffff20]"
         >
           <Sidebar />
           <Center />
         </main>
+        <br />
+        <div>
+          <Player />
+        </div>
       </div>
     </>
     
