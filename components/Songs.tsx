99cf6d9d6b26c1d855cbef945d6e2b57db07f2cd
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-22 11:57:50 -0400

    Utilize Spotify API response types

--- components/Songs.tsx
+++ components/Songs.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { useRecoilValue } from 'recoil';
-import { playlistState } from '../atoms/playlistAtoms';
 import Song from './Song';
 
-const Songs = () => {
-  const playlist: any = useRecoilValue(playlistState);
+const Songs = ({playlists}: {playlists: SpotifyApi.SinglePlaylistResponse}) => {
 
   return (
     <div className="flex flex-col space-y-1 text-gray-500">
-      {playlist?.tracks.items.map((track: any, i: number) => (
-        <Song key={track.track.id} track={track} order={i} />
+      {playlists?.tracks.items.map((item, i) => (
+        <Song key={item.track.id} track={item.track} order={i} />
       ))}
     </div>
   )
