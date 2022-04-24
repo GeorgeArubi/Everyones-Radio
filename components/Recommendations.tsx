c17f956b61f056aac3de7e8c13c5ca91a3f7ebb0
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-24 17:35:16 -0400

    Dummy data experiment

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -33,7 +33,45 @@ const Recommendations = (props: { selectPlaylist: any; }) => {
       })
     }
   }, [session, setPlaylistId, spotifyApi])
-  console.log("You picked playlist >>>", playlistId)
+  //console.log("You picked playlist >>>", playlistId)
+  
+  /*
+  // Dummy Data
+  useEffect(() => {
+    const testData: string[] = [
+      '1DB96uvtjyakkiDxD7M1Zh',
+      '7ozFIvz1bE1HPgQTUl5qfT',
+      '4VdVH8e7BY4ESSdxa9ehK5',
+      '6s2bP0um1MLKog2aVdlqHt',
+      '1Bi23brRTBv0YfrrTYeaPC',
+      '4BPUhIizaCtpybYPz5EhCL',
+      '6DokzCTQ0UGh0ItaZBZrXC',
+      '6rpGWCNHsDXtqNv38xXgNS',
+      '58c4OKKyny03xYbw1wVl40',
+      '25OZXSdB5cpVG8xLoSEjyU',
+      '19p2VsvSMpyhkv6Tvdo20e',
+      '3NCJ6ihYk8yChkkwkvJxGr',
+      '6MPXVrcMxxB1zXq3fCHbpS',
+      '25fGd8uIFS2nS5DVJSf26s',
+      '3QQYDqtqBIzvXbbWgoKcxR',
+      '41c31jLpWNQEctdxktBtHX',
+      '3SvOnq7F7S1ONLVKQDRr7F',
+      '7lD1Z8qDWH4AvguNiiEgov',
+    ]
+    const totalTestSet: any[] = []
+    if (spotifyApi.getAccessToken()) {
+      testData.forEach((testPlaylist) => {
+        spotifyApi.getPlaylist(testPlaylist).then(function(data) {
+          totalTestSet.push(data.body);
+          // Put playlists into state
+          const totalPlaylists = [].concat(...totalTestSet);
+          setPlaylists(totalPlaylists);
+          })
+      })
+    }  
+  }, [session, setPlaylistId, spotifyApi])
+ */
+  
 
   // Get playlist info to be passed to playlist component
   useEffect(() => {
