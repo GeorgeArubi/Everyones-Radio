fdff85af9ea8470915ec854d5e5495c336d3778a
DOGHOUSETV <arubigeorge0@gmail.com>
2022-04-24 18:26:49 -0400

    Remove unnecessary jargon

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,6 @@
 import {NextApiRequest} from 'next';
 import {NextRequest, NextResponse} from 'next/server';
 import {getToken} from 'next-auth/jwt';
-
 export async function middleware(req: NextRequest) {
   // Token will exist if user is logged in
   const token = await getToken({req: req as unknown as NextApiRequest, secret: process.env.JWT_SECRET!});
@@ -11,14 +10,12 @@ export async function middleware(req: NextRequest) {
   // 1) It's a request for next-auth session & provider fetching
   // 2) requesting public assets
   // 3) the token exists
-  if (pathname.includes('/api/auth') || token) {
+  if (pathname.includes('/api/auth') || pathname.includes('/assets') || token) {
     return NextResponse.next();
   }
 
   // Redirect them to login if they don't have token AND are requesting a protected route
-  const loginURL = req.nextUrl.clone()
-  loginURL.pathname = '/login'
   if (!token && pathname !== '/login') {
-    return NextResponse.redirect(loginURL);
+    return NextResponse.redirect('/login');
   }
 }
