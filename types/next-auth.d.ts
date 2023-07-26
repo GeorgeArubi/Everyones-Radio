96f617d5360ef6d97d234acbbe93eacd8c33efc7
DOGHOUSETV <arubigeorge0@gmail.com>
2023-07-26 11:51:56 -0400

    Use type assertion

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,8 +4,11 @@ declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+
   interface Session {
     accessToken?: string,
+    refreshToken?: string,
+    username?: string,
     error?: string
   }
 }
